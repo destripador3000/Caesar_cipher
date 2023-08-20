23f757132ef3446e316d90fdc5a8d44a6e8c78c1
destripador3000 <114034675+destripador3000@users.noreply.github.com>
2023-08-20 00:22:04 -0500

    Update rotate_.py

--- rotate_.py
+++ rotate_.py
@@ -3,23 +3,22 @@
 # 
 cifrado_1=["A","B","C","D","E","F","G","H","I","J","K","L","M"]
 cifrado_2=["N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-palabra=input("Escribe la palabra a cifrar: ")
-h=[]
-n=[]
-new=palabra.upper()
-for i in new:
-    if i in cifrado_1:
+def cifrado():
+    palabra=input("Escribe la palabra a cifrar: ")
+    h=[]
+    n=[]
+    new=palabra.upper()
+    for i in new:
+        if i in cifrado_1:
         
-        pola = cifrado_1.index(i)
-        h.append(cifrado_2[pola])
-    elif i in cifrado_2:
-        pola2=cifrado_2.index(i)
-        n.append(cifrado_1[pola2])
+            pola = cifrado_1.index(i)
+            h.append(cifrado_2[pola])
+        elif i in cifrado_2:
+            pola2=cifrado_2.index(i)
+            n.append(cifrado_1[pola2])
 
-cifrado=h + n
-c="".join(cifrado)
+    cifrado=h + n
+    c = "".join(cifrado)
+    print(c)
 
-#print(h)
-#print(n)        
-print(cifrado)
-print(c)
+cifrado()
